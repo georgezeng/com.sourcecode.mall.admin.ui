02559c7636c1da41ad3609881ccbf0a3cc856f8a
georgezeng <zzeng@bglcorp.com.au>
2019-07-22 15:15:15 +0800

    no message

--- src/view/goods/item/list.vue
+++ src/view/goods/item/list.vue
@@ -53,7 +53,9 @@
         columns: [
           {type: 'selection', width: 60, align: 'center'},
           {title: '名称', key: 'name', sortable: true, sortType: 'asc'},
-          {title: '编号', key: 'number', sortable: true},
+          {
+            title: '编号', key: 'number', sortable: true
+          },
           {title: '货号', key: 'code', sortable: true},
           {title: '最低价格', key: 'minPrice', sortable: true},
           {title: '最高价格', key: 'maxPrice', sortable: true},
