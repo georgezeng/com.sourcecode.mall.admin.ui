b344e129e1d21c433ea611940b0abfaae767af61
georgezeng <zzeng@bglcorp.com.au>
2019-09-17 09:18:21 +0800

    no message

--- src/view/promotion/client-activity/edit.vue
+++ src/view/promotion/client-activity/edit.vue
@@ -90,6 +90,7 @@
             this.loading = true
             API.save({
               ...this.form,
+              status: this.form.status.name,
               startTime: this.form.startTime + ':00',
               endTime: this.form.endTime + ':00',
             }).then(res => {
